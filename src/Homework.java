d3bae3cd965d213815486778f89123f6e307cb8d
Andrii Prokofiev <apapmind@gmail.com>
2019-03-03 10:41:32 +0100

    new_answers

---
+++ src/Homework.java
@@ -0,0 +1,9 @@
+public class Homework {
+   static int a;
+    public static void main(String[] args) {
+
+        System.out.println(a);
+
+    }
+}
+
---
+++ src/Pace.java
@@ -0,0 +1,12 @@
+class Pace {
+    float pace;
+    public static void  main (String[] args){
+        Pace runner1 = new Pace();
+        Pace runner2 = new Pace();
+        runner1.pace = 4.56f;
+        runner2.pace = 4.97f;
+        System.out.println(runner1);
+
+
+    }
+}
